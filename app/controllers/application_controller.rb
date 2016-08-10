91d4860e8f16dd25d8632aad522a7b6508863e2e
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2016-08-09 22:35:06 -0400

    Tuesday help session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,11 @@
 class ApplicationController < ActionController::Base
-  
+
   protect_from_forgery with: :exception
   helper_method :current_user
 
   def current_user
   	if @current_user.nil?
+      # Because of find_by, current_user is an object (the entire user's row)
   		@current_user = User.find_by(id: session[:user_id])
   	end
 
@@ -14,7 +15,7 @@ class ApplicationController < ActionController::Base
   def authorize_user
   	unless current_user
   		flash[:message] = 'Please log in or register'
-  		redirect_to '/signup_or_login'	
-    end  
+  		redirect_to '/signup_or_login'
+    end
   end
 end
